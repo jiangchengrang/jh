cb07cb10e6d899afed57cd10de8c03c5aa2fd7b6
jiangchengrang <1740053553@qq.com>
2018-11-16 17:39:03 +0800

    js

---
+++ JS/2018.11.16/JSON && AJAX/home.php
@@ -0,0 +1,13 @@
+
+<?php
+  header("content-type:text/html;charset=utf-8"); 
+  echo "今天礼拜五!!!<br/>";
+  //定义变量
+  $red="红色";
+  $isbool=true;
+  $arr=array('lucy','lily');
+  //输出值和数据类型
+  var_dump($arr);
+  //输出拼接 
+  echo "我喜欢吃<span style='color:red'>".$red."</span>的苹果";
+?>
---
+++ JS/2018.11.16/JSON && AJAX/index.php
@@ -0,0 +1,9 @@
+<?php
+//get接收相关参数
+ // $id=$_GET["id"];
+ // $name=$_GET["name"];
+
+$name=$_POST["name"];
+//输出
+  echo  $name;
+?>
